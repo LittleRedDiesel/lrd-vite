ac539d2cd8329e058141103fee80d4603b8a313a
LittleRedDiesel <harriet@littlereddiesel.com>
2023-09-29 16:09:22 +0100

    Content and icons updated, styling for components and content blocks amended

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,68 @@
-import { useState } from 'react'
 import "./assets/scss/_styles.scss";
+import codeIcon from './assets/svg/code.svg'
+import editContentIcon from './assets/svg/edit-content.svg'
+import hourglassIcon from './assets/svg/hourglass.svg'
+import searchIcon from './assets/svg/search.svg'
+import pencilIcon from './assets/svg/pencil.svg'
+import uploadDesktop from './assets/svg/upload-desktop.svg'
+import userCheckIcon from './assets/svg/user-check.svg'
 
 function App() {
   return (
 		<>
 			<div className="content">
-				<p>We're a creative web solutions agency with over a decade of experience providing websites for small and large businesses. We build fast, dynamic and accessible customer-oriented websites and deliver them on time. We are passionate about web accessibility and this is reflected in our work. </p>
+				<p>We're a creative web solutions agency with over a decade of experience providing websites for small and large businesses. We build fast and dynamic websites and deliver them on time. We are passionate about web accessibility and this is reflected in our work. </p>
 			</div>
 			<div className="content">
 				<h2>Services</h2>
-				<ul>
-					<li>
-						Web development
+				<ul className="icon-list">
+					<li className="icon-list__item">
+						<span>
+							<img src={codeIcon} className="icon" alt="Code icon" />
+							Web development
+						</span>
 						<p>We love coding and web languages and have years of experience working with them.</p>
 					</li>
-					<li>
-						Website hosting
-						<p>We can host and manage your website for you and automate backups so it never goes down.</p>
-					</li>
-					<li>
-						Content management systems (CMS)
+					<li className="icon-list__item">
+						<span>
+							<img src={editContentIcon} className="icon" alt="Bar chart icon" />
+							Content management systems (CMS)
+						</span>
 						<p>We can edit your site's content or enable you to do this yourself.</p>
 					</li>
-					<li>
-						Copywriting
+					<li className="icon-list__item">
+						<span>
+							<img src={uploadDesktop} className="icon" alt="Bar chart icon" />
+							Website hosting
+						</span>
+						<p>We can host and manage your website for you and automate backups so it never goes down.</p>
+					</li>
+					<li className="icon-list__item">
+						<span>
+							<img src={pencilIcon} className="icon" alt="Bar chart icon" />
+							Copywriting
+						</span>
 						<p>We understand the importance of good copy and will happily advise on this.</p>
 					</li>
-					<li>
-						Accessibility auditing
+					<li className="icon-list__item">
+						<span>
+							<img src={userCheckIcon} className="icon" alt="Bar chart icon" />
+							Accessibility auditing
+						</span>
 						<p>We can perform accessibility audits on your site to check it adheres to WCAG standards.</p>
 					</li>
-					<li>
-						Speed testing
+					<li className="icon-list__item">
+						<span>
+							<img src={hourglassIcon} className="icon" alt="Bar chart icon" />
+							Speed testing
+						</span>
 						<p>If your site is slow or underperforming we can make improvements.</p>
 					</li>
-					<li>
-						SEO auditing
+					<li className="icon-list__item">
+						<span>
+							<img src={searchIcon} className="icon" alt="Bar chart icon" />
+							SEO auditing
+						</span>
 						<p>We can perform SEO audits on your site to ensure your business is always visible.</p>
 					</li>
 				</ul>
